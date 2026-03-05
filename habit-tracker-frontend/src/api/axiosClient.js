08098376509e8ceb907fa55cea9823c715d391f5
UlisesF04 <ulifernandez2014@gmail.com>
2026-03-05 17:49:09 -0300

    V1.3.5.3

--- habit-tracker-frontend/src/api/axiosClient.js
+++ habit-tracker-frontend/src/api/axiosClient.js
@@ -10,4 +10,15 @@ axiosClient.interceptors.request.use((config) => {
   return config;
 });
 
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.clear();
+      window.location.href = "/auth";
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default axiosClient;
